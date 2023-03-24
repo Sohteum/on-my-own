1e2800519df47eeb3c716840f2fc98ff745ef270
Sohteum <thtwk1123@gmail.com>
2023-03-24 16:30:42 +0900

    챌린지

--- challenge/javascript/02 resize/src/index.js
+++ challenge/javascript/02 resize/src/index.js
@@ -1,28 +1,41 @@
-const h2 = document.querySelector("body h2");
+const body = document.querySelector("body");
+
+function winResize() {
+  const innerWidth = window.innerWidth;
+
+  if(innerWidth<=500) {
+   body.style.backgroundColor = "blue";
+} else if(innerWidth<=800 && innerWidth>500) {
+  body.style.backgroundColor = "pink";
+} else if (innerWidth>=1100 && innerWidth>800) {
+  body.style.backgroundColor = "yellow";
+} 
+}
+
+window.addEventListener("resize", winResize);
 
 
+/* window.addEventListener(`resize`, function() {
+  
+}); */
 
-function winw(windowResize) {
-  let winW = window.width
-  let result
-  if (winW < 800) {
-    result = h2.style.backgroundcolor = "blue";
-  } else if (winW < 1000) {
-    result = window.body.style.background = "pink";
-  } else if (winW < 1300)
-    result = window.body.style.background = "pink";
-} return result
+/* 
+const body = document.body;
 
-window.addEventListener("resize", windowResize);
-function winw(windowResize) {
-  let winW = window.width
-  let result
-  if (winW < 800) {
-    result = h2.style.backgroundcolor = "blue";
-  } else if (winW < 1000) {
-    result = window.body.style.background = "pink";
-  } else if (winW < 1300)
-    result = window.body.style.background = "pink";
-} return result
+const BIG_SCREEN = "bigScreen";
+const MEDIUM_SCREEN = "mediumScreen";
+const SMALL_SCREEN = "smallScreen";
 
-window.addEventListener("resize", windowResize);
+function winResize() {
+  const innerWidth = window.innerWidth;
+  if(innerWidth>="500") {
+   body.style.backgroundColor = "blue";
+} else if(innerWidth>="800") {
+  body.style.backgroundColor = "pink";
+} else if (innerWidth>="1100") {
+  body.style.backgroundColor = "yellow";
+} 
+}
+
+window.addEventListener("resize", handleResize);
+ */
