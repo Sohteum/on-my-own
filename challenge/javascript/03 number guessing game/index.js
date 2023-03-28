e91e4ac1383c50f8b29df68b65ad4fa83ab1bfe4
Sohteum <thtwk1123@gmail.com>
2023-03-28 23:47:15 +0900

    .

--- challenge/javascript/03 number guessing game/index.js
+++ challenge/javascript/03 number guessing game/index.js
@@ -1,17 +1,31 @@
 
+const gameForm = document.querySelector('.gameForm');
+const userInput = document.querySelector('num1');
+const youChosen = document.querySelector('.setNum span:first-child');
+const randomNum = document.querySelector('.setNum span:last-child');
+const result = document.querySelector('.result');
 
-const userSetNumber = document.querySelector(`.num1`).innerHTML
-const randomNumber = parseInt(Math.ceil(Math.random) * userSetNumber)
-
-document.querySelector(`div>p>button`).addEventListener(`click`, e => {
-  e.preventDefault()
-  e.eventTarget.innerHTML
-})
-
-
-function handlerRangeInput() {
-  const rangeNum = rangeInput.value;
-  chooseInput.setAttribute("max", rangeNum);
+function RandomN() {
+  const ranNum = Math.ceil(Math.random()) * parseInt(num1);
+  return ranNum;
 }
 
-rangeForm.addEventListener("input", handlerRangeInput);
+function buttonClick(e) {
+  e.preventDefault();
+  e.eventTarget.innerHTML;
+
+  let num1 = document.querySelector('.num1').value;
+  const userInput = Number(userInput.value);
+  const randomNum = RandomN(0, num1);
+
+  youChosen.innerText = `You chose:${userInput}`;
+  randomNum.innerText = `, machine chose: ${randomNum}`;
+
+  if (userInput === randomNum) {
+    result.innerText = "You won!";
+  } else {
+    result.innerText = "You lost!";
+  }
+}
+
+gameForm.addEventListener("input", buttonClick);
