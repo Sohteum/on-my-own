830f50aa1472b5542b91d9cebbc72953a8fe6989
Sohteum <thtwk1123@gmail.com>
2023-04-25 23:19:19 +0900

    .

---
+++ practicing/main.js
@@ -0,0 +1,8 @@
+// document.querySelector('button').addEventListener(`click`, e => {
+//   document.querySelector('div').classList.toggle('active');
+// })   
+
+
+document.querySelector('button').addEventListener(`click`, e => {
+  document.getElementById('box').className;
+})
