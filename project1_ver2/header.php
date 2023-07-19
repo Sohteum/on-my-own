e9f5ab2aeba92ff7ebc50c4a98c2e377bb7f4dd4
Sohteum <thtwk1123@gmail.com>
2023-07-19 17:40:15 +0900

    ver2

---
+++ project1_ver2/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ko">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://fonts.googleapis.com/css2?family=Nanum+Gothic:wght@400;700;800&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Nanum+Myeongjo:wght@400;700;800&display=swap" rel="stylesheet">
+  <script src="https://kit.fontawesome.com/10bf1730a7.js" crossorigin="anonymous"></script>
+
+  <link rel="stylesheet" href="./css/reset.css">
+  <link rel="stylesheet" href="./css/style.css">
+
+  <script src="https://code.jquery.com/jquery-latest.js"></script>
+  <title>IamGeaGeul</title>
+</head>
+
+<body>
+  <h1>
+    제목1
+  </h1>
+  <h2>
+    제목2
+  </h2>
+  <i style="color:red;" class="fa-solid fa-bars"></i>
